7f885e6e603c657cc10a113b4459ef19881e68c2
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-12-17 13:17:17 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,3 +5,14 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 30s;
 
+target=$(docker-compose port tooljet 80)
+
+curl http://${target}/api/setup-admin \
+  -H 'accept: */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6,zh-CN;q=0.5,zh;q=0.4' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' \
+  --data-raw '{"companyName":"admin","companySize":"1-10","role":"Other","name":"admin","workspace":"admin","email":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'"}' \
+  --compressed
